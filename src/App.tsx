852b45a8bd48b4b0a245772026ab4953c36f22ea
Dave Hrdlicka <dave.hrdlicka@gmail.com>
2023-03-07 14:24:57 +0100

    Fix mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,6 @@ const TECH_STACK_ICONS = [
   imgGraphQL,
   imgPostgres,
 ];
-const TECH_STACK_ICONS_RADIUS = 200;
 const YEARS_OF_EXPERIENCE = new Date().getUTCFullYear() - 2015;
 
 function App() {
@@ -40,75 +39,86 @@ function App() {
     <div>
       <img
         src={imgBackground}
-        className="h-screen w-screen object-cover object-bottom fixed -z-10"
+        className="min-h-screen h-[600px] w-screen object-cover object-bottom fixed -z-10"
       />
-      <section className="h-screen bg-transparent">
-        <div className="container h-full columns-2">
-          <div className="h-full flex flex-col justify-end pb-20">
+      <div className="bg-transparent">
+        <section className="container min-h-screen h-[600px] flex flex-col md:flex-row-reverse gap-20 items-center">
+          <div className="h-1/2 md:h-full md:w-2/3 flex flex-col justify-center">
+            <header className="self-center p-10 bg-slate-900">
+              <h1 className="font-extrabold text-3xl lg:text-6xl">
+                Hi! I'm Dave
+              </h1>
+              <p className="text-xl lg:text-3xl">
+                Welcome to my personal website!
+              </p>
+            </header>
+          </div>
+          <div className="md:h-full w-2/3 md:w-1/3 flex flex-col justify-end pb-20">
             <img
-              className="w-2/3 self-center"
+              className="self-center"
               src={imgHacker}
               ref={hackerImageRef}
               alt="Hacking Dave"
             />
           </div>
-          <div className="h-full flex flex-col justify-center">
-            <header className="self-center p-10 bg-slate-900">
-              <h1 className="font-extrabold text-6xl">Hi! I'm Dave</h1>
-              <p className="text-3xl">Welcome to my personal website!</p>
-            </header>
-          </div>
-        </div>
-      </section>
-      <div className="bg-slate-900 drop-shadow-[0px_500px_0px_rgba(15,23,42,1)] shadow-slate-900">
-        <section className="container h-screen flex gap-32 items-center">
-          <div className="w-1/3 relative">
-            <img src={imgBackpack} className="w-1/2 mx-auto" />
-            <div className="animate-spin-icons absolute top-1/2 left-1/2">
-              {TECH_STACK_ICONS.map((imgURL, i) => {
-                const x = TECH_STACK_ICONS_RADIUS * Math.cos(iconsAngle) - 40;
-                const y = TECH_STACK_ICONS_RADIUS * Math.sin(iconsAngle) - 40;
-                iconsAngle += angleIncrement;
-
-                return (
-                  <img
-                    key={imgURL}
-                    className={`absolute animate-spin-icons-rev w-20 max-w-none`}
-                    src={imgURL}
-                    style={{
-                      left: `${x}px`,
-                      top: `${y}px`,
-                    }}
-                  />
-                );
-              })}
-            </div>
-          </div>
-          <article className="w-2/3 py-10">
-            <p className="text-xl">
-              I am a versatile lead software developer with{" "}
-              <strong>over {YEARS_OF_EXPERIENCE} years of experience</strong>{" "}
-              across different stacks. Throughout my career, I've worked on a
-              range of projects, from small business presentation websites to
-              large-scale enterprise applications where not only functionality
-              but also maintainability and scalability were critical. My skills
-              and experience allow me to tackle complex problems and find
-              creative solutions that drive business growth and success. I
-              specialize in a variety of programming languages and tools,
-              including{" "}
-              <strong>
-                Python, TypeScript, Solidity, Postgres, Docker, Terraform, AWS,
-                REST and GraphQL APIs
-              </strong>{" "}
-              and more. I am always eager to learn new technologies and tools to
-              stay ahead of the curve.
-            </p>
-          </article>
         </section>
+      </div>
+      <div className="bg-slate-900 drop-shadow-[0px_500px_0px_rgba(15,23,42,1)] shadow-slate-900">
+        <div className="overflow-hidden">
+          <section className="container min-h-screen md:h-[600px] flex flex-col md:flex-row-reverse gap-20 items-center py-20">
+            <article className="md:w-2/3">
+              <p className="md:text-xl md:py-20">
+                I am a versatile lead software developer with{" "}
+                <strong>over {YEARS_OF_EXPERIENCE} years of experience</strong>{" "}
+                across different stacks. Throughout my career, I've worked on a
+                range of projects, from small business presentation websites to
+                large-scale enterprise applications where not only functionality
+                but also maintainability and scalability were critical. My
+                skills and experience allow me to tackle complex problems and
+                find creative solutions that drive business growth and success.
+                I specialize in a variety of programming languages and tools,
+                including{" "}
+                <strong>
+                  Python, TypeScript, Solidity, Postgres, Docker, Terraform,
+                  AWS, REST and GraphQL APIs
+                </strong>{" "}
+                and more. I am always eager to learn new technologies and tools
+                to stay ahead of the curve.
+              </p>
+            </article>
+            <div className="md:w-1/3 relative py-10">
+              <img src={imgBackpack} className="w-1/3 md:w-1/2 mx-auto" />
+              <div className="animate-spin-icons absolute top-1/2 left-1/2">
+                {TECH_STACK_ICONS.map((imgURL, i) => {
+                  const isMobile = window.innerWidth < 1024;
+                  const TECH_STACK_ICONS_RADIUS = isMobile ? 100 : 160;
+                  const iconOffset = isMobile ? 20 : 40;
+                  const x =
+                    TECH_STACK_ICONS_RADIUS * Math.cos(iconsAngle) - iconOffset;
+                  const y =
+                    TECH_STACK_ICONS_RADIUS * Math.sin(iconsAngle) - iconOffset;
+                  iconsAngle += angleIncrement;
+
+                  return (
+                    <img
+                      key={imgURL}
+                      className={`absolute animate-spin-icons-rev w-10 lg:w-20 max-w-none`}
+                      src={imgURL}
+                      style={{
+                        left: `${x}px`,
+                        top: `${y}px`,
+                      }}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+        </div>
         <div className="bg-fuchsia-900">
-          <section className="container h-screen flex gap-32 items-center">
-            <article className="w-2/3 py-10">
-              <p className="text-xl">
+          <section className="container min-h-screen md:h-[600px] flex flex-col md:flex-row gap-20 items-center py-20">
+            <article className="md:w-2/3">
+              <p className="md:text-xl md:py-20">
                 In addition to my development skills, I have experience{" "}
                 <strong>
                   mentoring junior developers and managing small teams
@@ -122,13 +132,13 @@ function App() {
                 project goals and has the resources they need to be successful.
               </p>
             </article>
-            <img src={imgTeam} className="w-1/3" />
+            <img src={imgTeam} className="md:w-1/3" />
           </section>
         </div>
-        <section className="container h-screen flex gap-32 items-center">
-          <img src={imgProfile} className="w-1/3" />
-          <article className="w-2/3 py-10">
-            <p className="text-2xl">
+        <section className="container min-h-screen md:h-[600px] flex flex-col md:flex-row gap-10 md:gap-20 items-center py-20">
+          <img src={imgProfile} className="md:w-1/3" />
+          <article className="md:w-2/3">
+            <p className="md:text-2xl md:py-20">
               Whether you're looking to build a new software application,
               improve an existing one, or troubleshoot a problem, I'm here to
               help.{" "}
